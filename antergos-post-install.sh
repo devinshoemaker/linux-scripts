8d9c06815d8d08750035ef2a6b4fa5038438c6ff
Devin Shoemaker <devinshoe@gmail.com>
2016-10-09 18:03:40 -0500

    antergos-post-install.sh

---
+++ antergos-post-install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# Rank mirrors
+sudo rm /etc/pacman.d/mirrorlist.backup
+sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+sudo sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
+sudo rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
+
+# Update packages
+yaourt -Syyuu
+
+# Install user applications
+yaourt -S google-chrome-beta gparted filezilla
+
+# Development tools
+yaourt -S atom android-studio gitkraken
