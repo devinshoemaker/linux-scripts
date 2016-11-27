9caa34db4d509a03528ba4100fcd8196ba0a8813
Devin Shoemaker <devinshoe@gmail.com>
2016-11-26 19:38:52 -0600

    Install Suse Image Writer

--- antergos-post-install.sh
+++ antergos-post-install.sh
@@ -13,7 +13,7 @@ yaourt -Syyuu
 yaourt -S noto-fonts-emoji
 
 # Install user applications
-yaourt -S calibre google-chrome gparted guake filezilla lm_sensors p7zip slack-desktop
+yaourt -S calibre filezilla google-chrome gparted guake imagewriter lm_sensors p7zip slack-desktop
 
 # Development tools
 yaourt -S atom android-studio gitkraken
