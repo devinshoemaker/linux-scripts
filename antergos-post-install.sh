e7fd2e5c9c5e4fec65bee5f40532de2eb9ac4862
Devin Shoemaker <devinshoe@gmail.com>
2016-12-01 18:12:48 -0600

    Fix commit

--- antergos-post-install.sh
+++ antergos-post-install.sh
@@ -13,7 +13,6 @@ yaourt -Syyuu
 yaourt -S noto-fonts-emoji
 
 # Install user applications
-yaourt -S calibre filezilla google-chrome gparted guake imagewriter lm_sensors p7zip slack-desktop
 yaourt -S calibre filezilla google-chrome gparted guake imagewriter jdk8-openjdk lm_sensors p7zip slack-desktop
 
 # Development tools
