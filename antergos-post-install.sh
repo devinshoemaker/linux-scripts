1201247fafc6e66744df857620f28b9dcab86a1e
Devin Shoemaker <devinshoe@gmail.com>
2016-12-01 18:11:06 -0600

    Instal OpenJDK 8

--- antergos-post-install.sh
+++ antergos-post-install.sh
@@ -14,6 +14,7 @@ yaourt -S noto-fonts-emoji
 
 # Install user applications
 yaourt -S calibre filezilla google-chrome gparted guake imagewriter lm_sensors p7zip slack-desktop
+yaourt -S calibre filezilla google-chrome gparted guake imagewriter jdk8-openjdk lm_sensors p7zip slack-desktop
 
 # Development tools
 yaourt -S atom android-studio gitkraken
