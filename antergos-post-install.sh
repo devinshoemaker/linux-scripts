b8e09eda062647f8e51bd16d72e2d6344ec9e026
Devin Shoemaker <devinshoe@gmail.com>
2016-12-15 17:53:33 -0600

    Remove rank mirrrors

--- antergos-post-install.sh
+++ antergos-post-install.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
 
-# Rank mirrors
-#sudo rm /etc/pacman.d/mirrorlist.backup
-#sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-#sudo sed -i 's/^#Server/Server/' /etc/pacman.d/mirrorlist.backup
-#sudo rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
-
 # Update packages
 yaourt -Syyuu
 
