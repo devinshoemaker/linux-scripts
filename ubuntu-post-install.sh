2da4b7d2c1338bba058ed41a53511338a9134900
Devin Shoemaker <devinshoe@gmail.com>
2017-06-10 10:50:25 -0500

    Ubuntu: install Google Chrome stable

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -6,5 +6,11 @@
 sudo apt-get update &&
 sudo apt-get upgrade
 
+## Repos
+
+# Google Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - &&
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' &&
+
 # Install User Applications
-sudo apt-get install gparted guake
+sudo apt-get install google-chrome-stable gparted guake
