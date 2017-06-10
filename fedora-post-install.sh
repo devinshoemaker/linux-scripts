3017f38672172182361bff906bb03a4701811c3a
Devin Shoemaker <devinshoe@gmail.com>
2017-06-09 20:21:00 -0500

    Fedora: install Atom I also created a new section called "Repos" and install Google Chrome with all the other user applications.

--- fedora-post-install.sh
+++ fedora-post-install.sh
@@ -6,6 +6,8 @@
 sudo dnf check-update &&
 sudo dnf upgrade &&
 
+## Repos
+
 # Google Chrome
 sudo cat << EOF > /etc/yum.repos.d/google-chrome.repo
 [google-chrome]
@@ -15,7 +17,9 @@ enabled=1
 gpgcheck=1
 gpgkey=https://dl-ssl.google.com/linux/linux_signing_key.pub
 EOF &&
-sudo dnf install google-chrome-stable &&
+
+# Atom
+sudo dnf copr enable mosquito/atom &&
 
 # Install user applications
-sudo dnf install gparted guake simple-scan
+sudo dnf install atom google-chrome-stable gparted guake simple-scan
