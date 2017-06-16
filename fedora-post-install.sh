b85d8dc36e2948c6b3f1a8a5907ed8c1908fde48
Devin Shoemaker <devinshoe@gmail.com>
2017-06-16 07:14:15 -0500

    Fedora: install repos manually with su

--- fedora-post-install.sh
+++ fedora-post-install.sh
@@ -2,27 +2,33 @@
 
 # Fedora 25 Post Install Script
 
-# Update Packages
-sudo dnf check-update &&
-sudo dnf upgrade &&
-
 ## Repos
+## Run manually first! Then remove before running script.
+
+su
 
 # Google Chrome
-sudo cat << EOF > /etc/yum.repos.d/google-chrome.repo
+
+cat << EOF > /etc/yum.repos.d/google-chrome.repo
 [google-chrome]
 name=google-chrome - \$basearch
 baseurl=http://dl.google.com/linux/chrome/rpm/stable/\$basearch
 enabled=1
 gpgcheck=1
 gpgkey=https://dl-ssl.google.com/linux/linux_signing_key.pub
-EOF &&
+EOF
+
+exit
+
+# Update Packages
+sudo dnf check-update
+sudo dnf upgrade
 
 # Atom
-sudo dnf copr enable mosquito/atom &&
+sudo dnf copr enable mosquito/atom
 
 # Install user applications
-sudo dnf install google-chrome-stable gparted guake simple-scan &&
+sudo dnf install google-chrome-stable gparted guake simple-scan
 
 # Install development tools
 sudo dnf install atom couchdb
