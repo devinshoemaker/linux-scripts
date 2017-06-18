0b17ae39043bb167df90bd87193e46c34f505a85
Devin Shoemaker <dshoemaker@localhost.localdomain>
2017-06-18 10:16:43 -0500

    Fedora: install IntelliJ Ulimate

--- fedora-post-install.sh
+++ fedora-post-install.sh
@@ -41,3 +41,34 @@ sudo dnf install atom couchdb vscode
 
 # .bashrc
 echo export EDITOR=nano >> ~/.bashrc
+
+# Install IntelliJ Ultimate
+
+## Download from website 
+wget https://download.jetbrains.com/idea/ideaIU-2017.1.4.tar.gz
+
+## Extract tar.gz
+tar -xzf idea*.tar.gz 
+cd idea*
+
+## Install to opt
+sudo mkdir -p /opt/jetbrains/idea
+sudo cp -r * /opt/jetbrains/idea/
+
+## Add to PATH
+export PATH=$PATH:/opt/jetbrains/idea/bin >> ~/.bashrc
+
+## Create desktop launcher
+cat << EOF > ~/.local/share/applications/intellij-idea-ultimate.desktop
+#!/usr/bin/env xdg-open
+[Desktop Entry]
+Terminal=false
+Type=Application
+Name=IntelliJ IDEA Ultimate
+Exec=/opt/jetbrains/idea/bin/idea.sh
+Icon=/opt/jetbrains/idea/bin/idea.png
+EOF
+
+## Clean up
+cd ../
+rm -rf idea*
