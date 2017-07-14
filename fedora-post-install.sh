a6f60eaec96ba78997a6a818ed1497140d90cdb9
Devin Shoemaker <devinshoe@gmail.com>
2017-07-13 20:48:14 -0500

    Fedora: remove duplicate line

--- fedora-post-install.sh
+++ fedora-post-install.sh
@@ -49,7 +49,6 @@ sudo dnf install xorg-x11-drv-nvidia akmod-nvidia "kernel-devel-uname-r == $(una
 sudo dnf install gstreamer-plugins-bad gstreamer-plugins-bad-free-extras gstreamer-plugins-bad-nonfree gstreamer-plugins-ugly gstreamer-ffmpeg gstreamer1-libav gstreamer1-plugins-bad-free-extras gstreamer1-plugins-bad-freeworld gstreamer1-plugins-base-tools gstreamer1-plugins-good-extras gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-good gstreamer1-plugins-base gstreamer1
 
 # Install user applications
-sudo dnf install arc-theme-solid arc-icon-theme flash-plugin alsa-plugins-pulseaudio libcurl fuse-exfat gnome-tweak-tool google-chrome-stable gparted guake nano pulseaudio-equalizer simple-scan spotify transmission
 sudo dnf install arc-theme-solid arc-icon-theme flash-plugin alsa-plugins-pulseaudio libcurl fuse-exfat gnome-tweak-tool google-chrome-stable gparted guake nano pulseaudio-equalizer simple-scan spotify transmission xclip
 
 # Install GNOME extensions
