87f38ec59991fe7bd6051aefb8a4e1514517580f
Devin Shoemaker <devinshoe@gmail.com>
2017-07-15 09:41:33 -0500

    fedora-install-disapora: initialize PostgreSQL database

--- fedora/fedora-install-diaspora.sh
+++ fedora/fedora-install-diaspora.sh
@@ -4,3 +4,6 @@
 
 # Install development packages
 sudo dnf install tar make automake gcc gcc-c++ git net-tools libcurl-devel libxml2-devel libffi-devel libxslt-devel wget redis ImageMagick nodejs postgresql-devel postgresql-server
+
+# Initialize PostgreSQL database
+sudo postgresql-setup initdb
