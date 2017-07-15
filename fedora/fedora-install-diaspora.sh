e2c15a012fd1d3f0c8137af45afd36e59c9eb435
Devin Shoemaker <devinshoe@gmail.com>
2017-07-15 09:41:04 -0500

    Fedora Install Diaspora: install development packages

--- fedora/fedora-install-diaspora.sh
+++ fedora/fedora-install-diaspora.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
 # Install Diaspora on Fedora 26 from source
+
+# Install development packages
+sudo dnf install tar make automake gcc gcc-c++ git net-tools libcurl-devel libxml2-devel libffi-devel libxslt-devel wget redis ImageMagick nodejs postgresql-devel postgresql-server
