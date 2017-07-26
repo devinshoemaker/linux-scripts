932cad7f669141fe4d6a16999e9d6a0fff0ef9a3
Devin Shoemaker <devinshoe@gmail.com>
2017-07-25 22:04:27 -0500

    fedy-post-install: update packages prior to installing Fedy

--- fedora/fedy-post-install.sh
+++ fedora/fedy-post-install.sh
@@ -2,8 +2,8 @@
 
 # Fedora 26 Post Install Script
 
-# Install Fedy
-sudo sh -c 'curl https://www.folkswithhats.org/installer | bash'
-
 # Update Packages
 sudo dnf upgrade --refresh
+
+# Install Fedy
+sudo sh -c 'curl https://www.folkswithhats.org/installer | bash'
