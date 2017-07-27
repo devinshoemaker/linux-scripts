e7946f3bb380fb6d55dbce704dec4668354b6736
Devin Shoemaker <devinshoe@gmail.com>
2017-07-26 20:22:00 -0500

    fedy-post-install: install free user applications

--- fedora/fedy-post-install.sh
+++ fedora/fedy-post-install.sh
@@ -5,5 +5,8 @@
 # Update Packages
 sudo dnf upgrade --refresh
 
+# Install Free User Applications
+sudo dnf install gnome-tweak-tool gparted guake nano pulseaudio-equalizer simple-scan transmission xclip
+
 # Install Fedy
 sudo sh -c 'curl https://www.folkswithhats.org/installer | bash'
