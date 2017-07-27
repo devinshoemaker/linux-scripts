89300f0feeff4acf6ef8784c3b91677d1dee6100
Devin Shoemaker <devinshoe@gmail.com>
2017-07-26 20:47:17 -0500

    fedora-install-gitkraken: add GitKraken icon

--- fedora/fedora-install-gitkraken.sh
+++ fedora/fedora-install-gitkraken.sh
@@ -14,6 +14,10 @@ tar -xvzf gitkraken-amd64.tar.gz -C "/opt/"
 ## Symlink to /usr/bin
 sudo ln -sf "/opt/gitkraken/gitkraken" "/usr/bin/gitkraken"
 
+## Add the GitKraken Icon to GTK
+sudo cp "gitkraken.svg" "/usr/share/icons/hicolor/scalable/apps/"
+sudo gtk-update-icon-cache -f -t /usr/share/icons/hicolor
+
 ## Create desktop launcher
 cat << EOF > ~/.local/share/applications/gitkraken.desktop
 #!/usr/bin/env xdg-open
@@ -22,6 +26,7 @@ Terminal=false
 Type=Application
 Name=GitKraken
 Exec=gitkraken
+Icon=gitkraken
 EOF
 
 # Clean up
