a63c6a1a1dff85117eeb69c4f796e35efc617d25
Devin Shoemaker <devinshoe@gmail.com>
2017-07-26 20:22:34 -0500

    fedy-post-install: install free GNOME extensions

--- fedora/fedy-post-install.sh
+++ fedora/fedy-post-install.sh
@@ -8,5 +8,8 @@ sudo dnf upgrade --refresh
 # Install Free User Applications
 sudo dnf install gnome-tweak-tool gparted guake nano pulseaudio-equalizer simple-scan transmission xclip
 
+# Install GNOME extensions
+sudo dnf install gnome-shell-extension-no-topleft-hot-corner gnome-shell-extension-simple-dock gnome-shell-extension-topicons-plus
+
 # Install Fedy
 sudo sh -c 'curl https://www.folkswithhats.org/installer | bash'
