d41b280d80bbf7cd954baf42628bd5a957d1cb87
Devin Shoemaker <devinshoe@gmail.com>
2017-08-05 07:05:38 -0500

    fedy-post-install: install media codecs

--- fedora/fedy-post-install.sh
+++ fedora/fedy-post-install.sh
@@ -20,6 +20,9 @@ sudo sed -i s/^SELINUX=.*$/SELINUX=permissive/g /etc/selinux/config
 # Install Fedy
 sudo sh -c 'curl https://www.folkswithhats.org/installer | bash'
 
+# Install media codecs
+sudo dnf install gstreamer-plugins-bad gstreamer-plugins-bad-free-extras gstreamer-plugins-bad-nonfree gstreamer-plugins-ugly gstreamer-ffmpeg gstreamer1-libav gstreamer1-plugins-bad-free-extras gstreamer1-plugins-bad-freeworld gstreamer1-plugins-base-tools gstreamer1-plugins-good-extras gstreamer1-plugins-ugly gstreamer1-plugins-bad-free gstreamer1-plugins-good gstreamer1-plugins-base gstreamer1
+
 # Install Google Chrome
 ./fedora-install-chrome.sh
 
