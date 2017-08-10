3e84082f48cc69ceb7198a60da610e233bac41e0
Devin Shoemaker <devinshoe@gmail.com>
2017-08-09 20:58:49 -0500

    ubuntu-post-install: misc updates

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -1,23 +1,22 @@
 #!/bin/sh
 
-# Ubuntu 16.04.2 Post Install Script
+# Ubuntu 16.04/17.04 Post Install Script
 
 # Update Packages
-sudo apt-get update &&
-sudo apt-get upgrade
-
-## Repos
-
-# Google Chrome
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - &&
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' &&
+sudo apt -y update &&
+sudo apt -y upgrade
 
 # Install User Applications
-sudo apt-get update &&
-sudo apt-get install google-chrome-stable gparted guake
+sudo apt -y install gparted guake
 
 # Install Development Tools
-sudo apt-get install couchdb
+sudo apt -y install couchdb
+
+# Install Google Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - &&
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' &&
+sudo apt -y update &&
+sudo apt -y install google-chrome-stable
 
 # Remove Packages No Longer Required
-sudo apt autoremove
+sudo apt -y autoremove
