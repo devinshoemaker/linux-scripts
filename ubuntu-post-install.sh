906fa9956e3ca1841e30d8c5ec7d0cf3900ce8bf
Devin Shoemaker <devinshoe@gmail.com>
2017-08-10 16:49:08 -0500

    ubuntu-post-install: enable GNOME shell extensions

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -11,6 +11,8 @@ sudo apt -y install gparted guake pulseaudio-equalizer xclip
 
 # Install GNOME Shell Extensions
 sudo apt -y install gnome-shell-extension-dashtodock gnome-shell-extension-top-icons-plus
+gnome-shell-extension-tool -e dashtodock
+gnome-shell-extension-tool -e top-icons-plus
 
 # Install Development Tools
 sudo apt -y install couchdb ubuntu-make
