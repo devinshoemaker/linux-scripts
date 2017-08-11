e0d9a9116548dec1573028b9f2d2f75e1788e921
Devin Shoemaker <devinshoe@gmail.com>
2017-08-10 19:57:01 -0500

    ubuntu-post-install: install Paper GTK theme

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -35,6 +35,10 @@ wget https://release.gitkraken.com/linux/gitkraken-amd64.deb &&
 sudo dpkg -i gitkraken-amd64.deb &&
 rm gitkraken-amd64.deb
 
+# Install Paper Icon Theme
+sudo add-apt-repository -y ppa:snwh/pulp &&
+sudo apt -y update &&
+sudo apt -y install paper-gtk-theme paper-icon-theme
+
 # Remove Packages No Longer Required
 sudo apt -y autoremove
-
