b8d24f645fd29c4655509b39306d290b96662a62
Devin Shoemaker <devinshoe@gmail.com>
2017-08-12 16:23:51 -0500

    ubuntu-post-install: install Google Chrome with new script

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -28,10 +28,7 @@ cp -r .IntelliJIdea* ~/
 umake ide visual-studio-code --accept-license
 
 # Install Google Chrome
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - &&
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list' &&
-sudo apt -y update &&
-sudo apt -y install google-chrome-stable
+./ubuntu-install-chrome.sh
 
 # Install GitKraken
 ./ubuntu-install-gitkraken.sh
