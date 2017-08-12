10d0e36d8916be524b25aae13c5bb3450e564a50
Devin Shoemaker <devinshoe@gmail.com>
2017-08-12 16:23:40 -0500

    ubuntu-install-chrome.sh

---
+++ ubuntu/ubuntu-install-chrome.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Ubuntu 17.04 Install GitKraken
+
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt -y update
+sudo apt -y install google-chrome-stable
