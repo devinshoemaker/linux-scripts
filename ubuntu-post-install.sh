7051c93a95132eae54e0a9813e6c6a3617fc009a
Devin Shoemaker <devinshoe@gmail.com>
2017-09-09 13:34:38 -0500

    ubuntu/development-tool.sh

--- ubuntu-post-install.sh
+++ ubuntu-post-install.sh
@@ -19,30 +19,7 @@ sudo snap install discord
 sudo apt -y install gnome-shell-extension-dashtodock gnome-shell-extension-top-icons-plus
 
 # Install Development Tools
-sudo apt -y install couchdb docker.io
-sudo systemctl stop couchdb
-sudo systemctl disable couchdb
-
-# Add User to Docker Group
-sudo usermod -aG docker $(whoami)
-
-# Install Ubuntu Make from PPA
-sudo add-apt-repository -y ppa:ubuntu-desktop/ubuntu-make &&
-sudo apt update &&
-sudo apt -y install ubuntu-make
-
-# Install IntelliJ Ultimate
-umake ide idea-ultimate
-cp -r .IntelliJIdea* ~/
-
-# Install Android Studio
-umake android --accept-license
-
-# Install Atom
-umake ide atom
-
-# Install Visual Studio Code
-umake ide visual-studio-code --accept-license
+./ubuntu/development-tools.sh
 
 # Install Google Chrome
 ./ubuntu/google-chrome.sh
