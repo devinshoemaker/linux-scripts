fcc2ba63eb0ddf7f4b6efba572d52b51cdc46729
Devin Shoemaker <devinshoe@gmail.com>
2017-09-14 20:37:31 -0500

    mastodon: disable Redis and PostgreSQL by default

--- ubuntu/mastodon-development-1.sh
+++ ubuntu/mastodon-development-1.sh
@@ -17,6 +17,8 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/source
 # Install dependencies
 sudo apt update
 sudo apt -y install imagemagick ffmpeg libpq-dev libxml2-dev libxslt1-dev file git g++ libprotobuf-dev protobuf-compiler pkg-config nodejs gcc-6 autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev nginx redis-server redis-tools postgresql postgresql-contrib nginx letsencrypt yarn libidn11-dev libicu-dev
+sudo systemctl disable redis
+sudo systemctl disable postgresql
 
 # Set Up rbenv and ruby-build
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
