bf82e5da8b488969458e1da5c67bac015d620147
Devin Shoemaker <devinshoe@gmail.com>
2017-09-26 07:22:36 -0500

    ubuntu/google-chrome: remove '-y' from apt update

--- ubuntu/google-chrome.sh
+++ ubuntu/google-chrome.sh
@@ -4,5 +4,5 @@
 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-sudo apt -y update
+sudo apt update
 sudo apt -y install google-chrome-stable
