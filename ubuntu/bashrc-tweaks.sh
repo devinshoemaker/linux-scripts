d8bce50ef81a2d0c7e8841ca85dd8054fad5ba87
Devin Shoemaker <devinshoe@gmail.com>
2017-09-26 07:22:16 -0500

    ubuntu/bashrc-tweaks: fix alias

--- ubuntu/bashrc-tweaks.sh
+++ ubuntu/bashrc-tweaks.sh
@@ -2,5 +2,5 @@
 
 # Ubuntu 17.04 .bashrc Tweaks
 
-echo alias update='sudo apt update && apt list --upgradable' >> ~/.bashrc
-echo 'alias upgrade="sudo apt -y upgrade"' >> ~/.bashrc
+echo 'alias update="sudo apt update && apt list --upgradable"' >> ~/.bashrc
+echo 'alias upgrade="sudo apt -y upgrade && sudo snap refresh"' >> ~/.bashrc
