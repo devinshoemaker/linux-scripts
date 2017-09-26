b363af73b5ae95e7848f13fd7dcbef4137e90984
Devin Shoemaker <devinshoe@gmail.com>
2017-09-26 07:21:50 -0500

    ubuntu/arc-theme: add comments

--- ubuntu/arc-theme.sh
+++ ubuntu/arc-theme.sh
@@ -2,6 +2,11 @@
 
 # Ubuntu 17.04 Install Arc Theme
 
+# Add Repository
 sudo add-apt-repository -y ppa:noobslab/icons
+
+# Update Repositories
 sudo apt update
+
+# Install Theme
 sudo apt -y install arc-theme arc-icons
