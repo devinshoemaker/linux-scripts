6c9f39be584c0d96a6453298fd363a5d7c180e01
Devin Shoemaker <devinshoe@gmail.com>
2017-10-19 21:33:07 -0500

    ubuntu: remove "17.04" from most scripts

--- ubuntu/google-chrome.sh
+++ ubuntu/google-chrome.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Ubuntu 17.04 Install Google Chrome
+# Ubuntu Install Google Chrome
 
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
--- ubuntu/paper-theme.sh
+++ ubuntu/paper-theme.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Ubuntu 17.04 Install Paper Theme
+# Ubuntu Install Paper Theme
 
 sudo add-apt-repository -y ppa:snwh/pulp
 sudo apt update
