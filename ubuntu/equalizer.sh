1795b6f857c7e1fa79c8475ee96f3440d1e07bb3
Devin Shoemaker <devinshoe@gmail.com>
2017-10-19 21:17:51 -0500

    ubuntu/equalizer: misc updates

--- ubuntu/equalizer.sh
+++ ubuntu/equalizer.sh
@@ -1,8 +1,14 @@
 #!/bin/sh
 
-# Ubuntu 17.10 Install PulseAudio Equalizer
+# Ubuntu Install PulseAudio Equalizer
 
+# Update Repositories
+sudo apt update
+
+# Install Package
 sudo apt -y install pulseaudio-equalizer
+
+# Fix Equalizer
 sudo passwd root
 su -c 'echo load-module module-equalizer-sink  >> /etc/pulse/default.pa
 echo load-module module-dbus-protocol >> /etc/pulse/default.pa'
