7f2c8db1c2fccca842e0bcdf9e81fd8a3595d558
Devin Shoemaker <devinshoe@gmail.com>
2017-10-30 21:43:32 -0500

    ubuntu/development-tools: misc updates

--- ubuntu/16.04/development-tools.sh
+++ ubuntu/16.04/development-tools.sh
@@ -5,18 +5,13 @@
 # Update Repositories
 sudo apt update
 
-# Install Packages
-sudo apt install -y couchdb
-sudo systemctl stop couchdb
-sudo systemctl disable couchdb
-
-# Install Docker
-./docker.sh
-
 # Install Ubuntu Make from PPA
 sudo add-apt-repository -y ppa:ubuntu-desktop/ubuntu-make
 sudo apt update
 sudo apt install -y ubuntu-make
+
+# Install Atom
+umake ide atom $HOME/.local/share/umake/ide/atom
 
 # Install IntelliJ Ultimate
 umake ide idea-ultimate $HOME/.local/share/umake/ide/idea-ultimate
@@ -25,14 +20,19 @@ cp -r .IntelliJIdea* ~/
 # Install Android Studio
 umake android $HOME/.local/share/umake/android/android-studio --accept-license
 
-# Install Atom
-umake ide atom $HOME/.local/share/umake/ide/atom
-
 # Install Visual Studio Code
-umake ide visual-studio-code $HOME/.local/share/umake/ide/visual-studio-code --accept-license
+#umake ide visual-studio-code $HOME/.local/share/umake/ide/visual-studio-code --accept-license
 
 # Install GitKraken
 ./gitkraken.sh
+
+# Install Packages
+sudo apt install -y couchdb
+sudo systemctl stop couchdb
+sudo systemctl disable couchdb
+
+# Install Docker
+./docker.sh
 
 # Install Angular, Ionic, and Download Source Code
 ./angular-ionic.sh
--- ubuntu/17.10/development-tools.sh
+++ ubuntu/17.10/development-tools.sh
@@ -5,6 +5,29 @@
 # Update Repositories
 sudo apt update
 
+# Install Ubuntu Make from PPA
+# This PPA is not available on 17.10 yet.
+#sudo add-apt-repository -y ppa:ubuntu-desktop/ubuntu-make
+#sudo apt update
+sudo apt install -y ubuntu-make
+
+# Install Atom
+umake ide atom $HOME/.local/share/umake/ide/atom
+
+# Install IntelliJ Ultimate
+umake ide idea-ultimate $HOME/.local/share/umake/ide/idea-ultimate
+cp -r ../../.IntelliJIdea* ~/
+
+# Install Android Studio
+# This does not install with the current version of umake.
+#umake android $HOME/.local/share/umake/android/android-studio --accept-license
+
+# Install Visual Studio Code
+#umake ide visual-studio-code $HOME/.local/share/umake/ide/visual-studio-code --accept-license
+
+# Install GitKraken
+./gitkraken.sh
+
 # Install Packages
 sudo apt install -y couchdb
 sudo systemctl stop couchdb
@@ -12,27 +35,6 @@ sudo systemctl disable couchdb
 
 # Install Docker
 ./docker.sh
-
-# Install Ubuntu Make from PPA
-sudo add-apt-repository -y ppa:ubuntu-desktop/ubuntu-make
-sudo apt update
-sudo apt install -y ubuntu-make
-
-# Install IntelliJ Ultimate
-umake ide idea-ultimate $HOME/.local/share/umake/ide/idea-ultimate
-cp -r .IntelliJIdea* ~/
-
-# Install Android Studio
-umake android $HOME/.local/share/umake/android/android-studio --accept-license
-
-# Install Atom
-umake ide atom $HOME/.local/share/umake/ide/atom
-
-# Install Visual Studio Code
-umake ide visual-studio-code $HOME/.local/share/umake/ide/visual-studio-code --accept-license
-
-# Install GitKraken
-./gitkraken.sh
 
 # Install Angular, Ionic, and Download Source Code
 ./angular-ionic.sh
