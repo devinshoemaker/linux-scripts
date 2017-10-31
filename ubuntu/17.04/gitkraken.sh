1051aa6099d7a90d50c1aeb29157ff594b553c4a
Devin Shoemaker <devinshoe@gmail.com>
2017-10-30 21:08:18 -0500

    ubuntu/17.10/gitkraken: install required dependencies

--- ubuntu/17.04/gitkraken.sh
+++ ubuntu/17.04/gitkraken.sh
@@ -2,6 +2,11 @@
 
 # Ubuntu Install GitKraken
 
+# Download Package
 wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
+
+# Install Package
 sudo dpkg -i gitkraken-amd64.deb
+
+# Clean Up
 rm gitkraken-amd64.deb
--- ubuntu/17.10/gitkraken.sh
+++ ubuntu/17.10/gitkraken.sh
@@ -2,6 +2,17 @@
 
 # Ubuntu 17.10 Install GitKraken
 
+# Update Repositories
+sudo apt update
+
+# Install Required Dependencies
+sudo apt install -y gconf2 python
+
+# Download Package
 wget https://release.gitkraken.com/linux/gitkraken-amd64.deb
+
+# Install Package
 sudo dpkg -i gitkraken-amd64.deb
+
+# Clean Up
 rm gitkraken-amd64.deb
