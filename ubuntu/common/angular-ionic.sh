064601a4d1e99874f85b93abd97ffa4e491736bc
Devin Shoemaker <devinshoe@gmail.com>
2017-12-12 17:21:37 -0600

    ubuntu/angular-ionic: clone Water Watch source code

--- ubuntu/common/angular-ionic.sh
+++ ubuntu/common/angular-ionic.sh
@@ -24,12 +24,14 @@ cd ~/code/angular
 
 git clone https://github.com/Dshoe/angular-tour-of-heroes
 
+# Add CORS to CouchDB
+sudo npm install -g add-cors-to-couchdb
+add-cors-to-couchdb
+
 # Download Ionic Source Code
 mkdir -p ~/code/ionic
 cd ~/code/ionic
 
 git clone https://github.com/dshoe/taskit-ionic.git
-sudo npm install -g add-cors-to-couchdb
-add-cors-to-couchdb
-
 git clone https://github.com/dshoe/coinbase-profit.git
+git clone https://github.com/dshoe/WaterWatch.git
