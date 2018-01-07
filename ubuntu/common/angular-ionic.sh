90652a8c12a20232c7abe9fe020490fd47dc8003
Devin Shoemaker <devinshoe@gmail.com>
2018-01-07 09:05:07 -0600

    ubuntu/common/angular-ionic: clone Crypto Profit Log

--- ubuntu/common/angular-ionic.sh
+++ ubuntu/common/angular-ionic.sh
@@ -33,5 +33,5 @@ mkdir -p ~/code/ionic
 cd ~/code/ionic
 
 git clone https://github.com/dshoe/taskit-ionic.git
-git clone https://github.com/dshoe/coinbase-profit.git
+git clone https://github.com/dshoe/CryptoProfitLog.git
 git clone https://github.com/dshoe/WaterWatch.git
