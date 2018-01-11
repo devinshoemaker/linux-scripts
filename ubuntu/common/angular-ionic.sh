fb26b89f9c1122763d8f229bca6d4b744c264380
Devin Shoemaker <devinshoe@gmail.com>
2018-01-11 11:07:21 -0600

    ubuntu/common/angular-ionic: clone Days Since Ionic project

--- ubuntu/common/angular-ionic.sh
+++ ubuntu/common/angular-ionic.sh
@@ -34,4 +34,5 @@ cd ~/code/ionic
 
 git clone https://github.com/dshoe/taskit-ionic.git
 git clone https://github.com/dshoe/CryptoProfitLog.git
+git clone https://github.com/dshoe/DaysSince.git
 git clone https://github.com/dshoe/WaterWatch.git
