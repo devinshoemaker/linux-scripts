01f4012d3b888a87f33c5abce05bd4a3f6ad667a
Devin Shoemaker <devinshoe@gmail.com>
2018-01-13 11:37:55 -0600

    ubuntu/common: etcher.sh

---
+++ ubuntu/common/etcher.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Install Etcher
+
+echo "deb https://dl.bintray.com/resin-io/debian stable etcher" | sudo tee /etc/apt/sources.list.d/etcher.list
+sudo apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 379CE192D401AB61
+sudo apt update
+sudo apt install -y etcher-electron
