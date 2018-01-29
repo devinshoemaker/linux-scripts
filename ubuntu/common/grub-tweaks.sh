bd17444789b5a48caf2b617dc110a8cf45a8769c
Devin Shoemaker <devinshoe@gmail.com>
2018-01-29 06:36:45 -0600

    ubuntu/common: Add GRUB tweaks script

---
+++ ubuntu/common/grub-tweaks.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Ubuntu - GRUB Tweaks
+
+# Remeber Last Selection
+sudo sh -c 'echo "GRUB_DEFAULT=saved" >> /etc/default/grub'
+sudo sh -c 'echo "GRUB_SAVEDEFAULT=true" >> /etc/default/grub'
+
+# Update GRUB
+sudo update-grub
