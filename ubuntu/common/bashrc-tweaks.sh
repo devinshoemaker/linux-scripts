c0a3e60fb85b4e161f3906cce8c3ffc204d8b72b
Devin Shoemaker <devinshoe@gmail.com>
2018-01-30 06:37:15 -0600

    ubuntu/common/bashrc-tweaks: Don't update NPM packages with `sudo`

--- ubuntu/common/bashrc-tweaks.sh
+++ ubuntu/common/bashrc-tweaks.sh
@@ -3,4 +3,4 @@
 # Ubuntu .bashrc Tweaks
 
 echo 'alias update="sudo apt update && apt list --upgradable"' >> ~/.bashrc
-echo 'alias upgrade="sudo apt -y upgrade && sudo snap refresh && sudo npm update -g"' >> ~/.bashrc
+echo 'alias upgrade="sudo apt -y upgrade && sudo snap refresh && npm update -g"' >> ~/.bashrc
