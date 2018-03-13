9dc3e651613b186441823bea15c4e056ef53cfda
Devin Shoemaker <devinshoe@gmail.com>
2018-03-13 06:54:57 -0500

    ubuntu/common/bashrc-tweaks: add new line before bashrc additions

--- ubuntu/common/bashrc-tweaks.sh
+++ ubuntu/common/bashrc-tweaks.sh
@@ -2,5 +2,6 @@
 
 # Ubuntu .bashrc Tweaks
 
+echo '' >> ~/.bashrc
 echo 'alias update="sudo apt update && apt list --upgradable"' >> ~/.bashrc
 echo 'alias upgrade="sudo apt -y upgrade && sudo snap refresh && npm update -g"' >> ~/.bashrc
