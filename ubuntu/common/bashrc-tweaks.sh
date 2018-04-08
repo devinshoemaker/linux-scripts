e5afd319743e31357ae3bb14406b87e117318214
Devin Shoemaker <devinshoe@gmail.com>
2018-04-08 17:31:59 -0500

    ubuntu/common/bashrc-tweaks: modify `upgrade` alias

--- ubuntu/common/bashrc-tweaks.sh
+++ ubuntu/common/bashrc-tweaks.sh
@@ -7,4 +7,4 @@ set -e
 
 echo '' >> ~/.bashrc
 echo 'alias update="sudo apt update && apt list --upgradable"' >> ~/.bashrc
-echo 'alias upgrade="sudo apt -y upgrade && sudo snap refresh && npm update -g"' >> ~/.bashrc
+echo 'alias upgrade="sudo apt -y upgrade"' >> ~/.bashrc
