d9ef19990330a96a9ad04554150637b49b393c7d
Devin Shoemaker <devinshoe@gmail.com>
2018-04-11 15:53:58 -0500

    common/development-snaps: misc updates

--- common/development-snaps.sh
+++ common/development-snaps.sh
@@ -3,13 +3,13 @@
 # Development Oriented Snap Applications
 
 # IntelliJ IDEA Ultimate
-sudo snap install intellij-idea-ultimate --classic
+sudo snap install --classic intellij-idea-ultimate
 
 # Visual Studio Code
-sudo snap install vscode --classic
+sudo snap install --classic vscode
 
 # Atom
-sudo snap install atom --classic
+sudo snap install --classic atom
 
 # GitKraken
 sudo snap install --classic gitkraken
