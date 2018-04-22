ba93f82824409e5e9f0404e941d178c53fd3d0af
Devin Shoemaker <devinshoe@gmail.com>
2018-04-21 22:11:11 -0500

    common/user-snaps: install Telegram

--- common/user-snaps.sh
+++ common/user-snaps.sh
@@ -2,14 +2,17 @@
 
 # User Oriented Snap Applications
 
+# Telegram
+sudo snap install telegram-desktop
+
+# Discord
+sudo snap install discord
+
 # Google Play Music Desktop Player
 sudo snap install google-play-music-desktop-player
 
 # Spotify
 sudo snap install spotify
-
-# Discord
-sudo snap install discord
 
 # Slack
 sudo snap install --classic slack
