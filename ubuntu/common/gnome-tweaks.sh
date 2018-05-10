4a6a55e8f9e4d65c8213bb8712779d626a6ae998
Devin Shoemaker <devinshoe@gmail.com>
2018-05-09 22:11:47 -0500

    ubuntu/common/gnome-tweaks: install Pop!_OS theme

--- ubuntu/common/gnome-tweaks.sh
+++ ubuntu/common/gnome-tweaks.sh
@@ -5,6 +5,9 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
+# Store a local variable of the scripts current directory
+UBUNTU_COMMON_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 # Update Repositories
 sudo apt update
 
@@ -38,8 +41,16 @@ sudo apt -y install gnome-shell-extensions gnome-shell-extension-top-icons-plus
 # Install Arc Theme
 sudo apt -y install arc-theme
 
+# Install Pop!_OS Theme
+. ${UBUNTU_COMMON_DIR}/pop_os-theme.sh
+
 # Install Numix Icon Theme
 sudo apt -y install numix-icon-theme
+
+# Install Moka Icon Theme
+sudo add-apt-repository -y ppa:moka/daily
+sudo apt update
+sudo apt -y install moka-icon-theme
 
 # Change GDM Theme
 sudo update-alternatives --config gdm3.css
