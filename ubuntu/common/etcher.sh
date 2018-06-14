4787f62deff2420d8489143f65c42b56e2c8bcf1
Devin Shoemaker <devinshoe@gmail.com>
2018-06-14 17:56:55 -0500

    ubuntu/common/etcher: update key server
    Due to reliability issues with pgp.mit.edu, Resin.io has moved their key server to keyserver.ubuntu.com.
    
    https://github.com/resin-io/etcher

--- ubuntu/common/etcher.sh
+++ ubuntu/common/etcher.sh
@@ -6,6 +6,6 @@
 set -e
 
 echo "deb https://dl.bintray.com/resin-io/debian stable etcher" | sudo tee /etc/apt/sources.list.d/etcher.list
-sudo apt-key adv --keyserver hkp://pgp.mit.edu:80 --recv-keys 379CE192D401AB61
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
 sudo apt update
 sudo apt -y install etcher-electron
