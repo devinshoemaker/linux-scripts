16bcb38c3cdbba4a474eec0b7b264ae22ec9c38b
Devin Shoemaker <devinshoe@gmail.com>
2018-08-11 23:28:47 -0500

    ubuntu/18.04/bootstrap: misc text updates

--- ubuntu/18.04/bootstrap.sh
+++ ubuntu/18.04/bootstrap.sh
@@ -12,112 +12,112 @@ sudo -v
 UBUNTU_1804_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Update packages
-echo '==> Update packages? (Y/n)'
+echo "==> Update packages? (Y/n)"
 read USER_PROMPT
 if [ "$USER_PROMPT" = 'y' ] || [ "$USER_PROMPT" = 'Y' ] || [ "$USER_PROMPT" = '' ]; then
     sudo apt update
     sudo apt -y full-upgrade
 else
-    echo '==> Skipping package updates.'
+    echo "==> Skipping package updates."
 fi
 
 # Set root password
-echo '==> Set root password? (Y/n)'
+echo "==> Set root password? (Y/n)"
 read USER_PROMPT
 if [ "$USER_PROMPT" = 'y' ] || [ "$USER_PROMPT" = 'Y' ] || [ "$USER_PROMPT" = '' ]; then
     sudo passwd root
 else
-    echo '==> Skipping root password.'
+    echo "==> Skipping root password."
 fi
 
 # GRUB tweaks
-echo '==> Install GRUB tweaks? (Y/n)'
+echo "==> Install GRUB tweaks? (Y/n)"
 read USER_PROMPT
 if [ "$USER_PROMPT" = 'y' ] || [ "$USER_PROMPT" = 'Y' ] || [ "$USER_PROMPT" = '' ]; then
     . ${UBUNTU_1804_DIR}/../common/grub-tweaks.sh
 else
-    echo '==> Skipping GRUB tweaks.'
+    echo "==> Skipping GRUB tweaks."
 fi
 
 # Install PulseAudio Equalizer
-echo '==> Install PulseAudio Equalizer? (Y/n)'
+echo "==> Install PulseAudio Equalizer? (Y/n)"
 read USER_PROMPT
 if [ "$USER_PROMPT" = 'y' ] || [ "$USER_PROMPT" = 'Y' ] || [ "$USER_PROMPT" = '' ]; then
     . ${UBUNTU_1804_DIR}/../common/equalizer.sh
 else
-    echo '==> Skipping PulseAudio Equalizer.'
+    echo "==> Skipping PulseAudio Equalizer."
 fi
 
 # GNOME Shell tweaks
-echo '==> Install GNOME tweaks, themes, and extensions? (y/N)'
+echo "==> Install GNOME tweaks, themes, and extensions? (y/N)"
 read USER_PROMPT
 if [ "$USER_PROMPT" = 'y' ] || [ "$USER_PROMPT" = 'Y' ]; then
     . ${UBUNTU_1804_DIR}/../common/gnome-tweaks.sh
 else
-    echo '==> Skipping GNOME tweaks.'
+    echo "==> Skipping GNOME tweaks."
 fi
 
 # .bashrc
-echo '==> Install ~/.bashrc modifications? (Y/n)'
+echo "==> Install ~/.bashrc modifications? (Y/n)"
 read USER_PROMPT
 if [ "$USER_PROMPT" = 'y' ] || [ "$USER_PROMPT" = 'Y' ] || [ "$USER_PROMPT" = '' ]; then
     . ${UBUNTU_1804_DIR}/../common/bashrc-tweaks.sh
 else
-    echo '==> Skipping ~/.bashrc tweaks.'
+    echo "==> Skipping ~/.bashrc tweaks."
 fi
 
 # Install NVIDIA driver
-echo '==> Install NVIDIA graphics driver? (y/N)'
+echo "==> Install NVIDIA graphics driver? (y/N)"
 read USER_PROMPT
 if [ "$USER_PROMPT" = 'y' ] || [ "$USER_PROMPT" = 'Y' ]; then
     sudo ubuntu-drivers autoinstall
 else
-    echo '==> Skipping NVIDIA driver.'
+    echo "==> Skipping NVIDIA driver."
 fi
 
 # Install user applications
-echo '==> Install basic packages? (Y/n)'
+echo "==> Install basic packages? (Y/n)"
 read USER_PROMPT
 if [ "$USER_PROMPT" = 'y' ] || [ "$USER_PROMPT" = 'Y' ] || [ "$USER_PROMPT" = '' ]; then
     sudo apt -y install exfat-fuse gimp gparted guake transmission-gtk virtualbox xclip
 else
-    echo '==> Skipping basic packages.'
+    echo "==> Skipping basic packages."
 fi
 
 # Install user oriented Snaps
-echo '==> Install user Snap packages? (Y/n)'
+echo "==> Install user Snap packages? (Y/n)"
 read USER_PROMPT
 if [ "$USER_PROMPT" = 'y' ] || [ "$USER_PROMPT" = 'Y' ] || [ "$USER_PROMPT" = '' ]; then
     . ${UBUNTU_1804_DIR}/../../common/user-snaps.sh
 else
-    echo '==> Skipping user Snap packages.'
+    echo "==> Skipping user Snap packages."
 fi
 
 # Install Google Chrome
-echo '==> Install Google Chrome? (Y/n)'
+echo "==> Install Google Chrome? (Y/n)"
 read USER_PROMPT
 if [ "$USER_PROMPT" = 'y' ] || [ "$USER_PROMPT" = 'Y' ] || [ "$USER_PROMPT" = '' ]; then
     . ${UBUNTU_1804_DIR}/../common/google-chrome.sh
 else
-    echo '==> Skipping Google Chrome.'
+    echo "==> Skipping Google Chrome."
 fi
 
 # Install Etcher
-echo '==> Install Etcher? (Y/n)'
+echo "==> Install Etcher? (Y/n)"
 read USER_PROMPT
 if [ "$USER_PROMPT" = 'y' ] || [ "$USER_PROMPT" = 'Y' ] || [ "$USER_PROMPT" = '' ]; then
     . ${UBUNTU_1804_DIR}/../common/etcher.sh
 else
-    echo '==> Skipping Etcher.'
+    echo "==> Skipping Etcher."
 fi
 
 # Install development tools
-echo '==> Install development tools? (Y/n)'
+echo "==> Install development tools? (Y/n)"
 read USER_PROMPT
 if [ "$USER_PROMPT" = 'y' ] || [ "$USER_PROMPT" = 'Y' ] || [ "$USER_PROMPT" = '' ]; then
     . ${UBUNTU_1804_DIR}/../common/development-tools.sh
 else
-    echo '==> Skipping development tools.'
+    echo "==> Skipping development tools."
 fi
 
 # Remove packages no longer required
