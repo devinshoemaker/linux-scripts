819f132e2bac78e3d6654a880ca8995f9840aef0
Devin Shoemaker <devinshoe@gmail.com>
2018-08-16 06:36:10 -0500

    common/ionic-projects: add `couch-blog` project

--- common/ionic-projects.sh
+++ common/ionic-projects.sh
@@ -6,7 +6,10 @@
 mkdir -p ~/Projects/ionic
 cd ~/Projects/ionic
 
-# Clone Ionic Projects
+# Personal Projects
 git clone git@github.com:dshoe/crypto-profit-log.git
 git clone git@github.com:dshoe/cube-coach.git
 git clone git@github.com:dshoe/taskit-ionic.git
+
+# Example Projects
+git clone git@github.com:dshoe/couch-blog.git
